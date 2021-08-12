ecee6399bb096e38bc684559f69ddf8ddcd11983
LeeProgrammer <jaeho010718@gmail.com>
2021-08-12 15:11:46 +0900

    Initial Commit

---
+++ SliderMusic/SliderMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderMusicApp.swift
+//  SliderMusic
+//
+//  Created by Lee Jaeho on 2021/08/12.
+//
+
+import SwiftUI
+
+@main
+struct SliderMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
